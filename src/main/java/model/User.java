9d2a526b0a47931d8732d794a2ce7db86303c480
jiajie <jiajie.ma@thoughtworks.com>
2020-10-21 12:40:01 +0800

    add user bean and some dependencies.

---
+++ src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private long id;
+    private String name;
+    private long age;
+    private String avatar;
+    private String description;
+}
