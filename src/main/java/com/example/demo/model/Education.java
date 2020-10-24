ceaeb5cbe4a80fff07696fceacd217d8d7517034
jiajie <jiajie.ma@thoughtworks.com>
2020-10-25 02:23:20 +0800

    [Jiajie]reinit repo and add configuration

--- src/main/java/com/example/demo/model/Education.java
+++ src/main/java/com/example/demo/model/Education.java
@@ -1,20 +1,24 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Education {
 
     @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userId;
 
     @NotNull
@@ -27,4 +31,7 @@ public class Education {
     @NotNull
     @Length(min = 1, max = 4096, message = "description长度不合法")
     private String description;
+
+    @ManyToOne
+    private User user;
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,20 +2,27 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User {
     @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotNull
