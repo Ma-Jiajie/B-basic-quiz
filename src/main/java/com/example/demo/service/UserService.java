9fe50d1273a7ddb58aed86b9444649ac9ae1572c
jiajie <jiajie.ma@thoughtworks.com>
2020-10-25 03:01:24 +0800

    [Jiajie]add UserService

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.selfexception.UserNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class UserService {
+    private final AtomicLong userIdSeq = new AtomicLong();
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findOneById(id)
+                .orElseThrow(() -> new UserNotFoundException("User Not Found!"));
+    }
+
+    public User createUser(User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+}
