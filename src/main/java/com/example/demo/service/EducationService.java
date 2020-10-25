396e1a88acfaeaf61900765708bbc5258f1e2e31
jiajie <jiajie.ma@thoughtworks.com>
2020-10-25 21:45:18 +0800

    [Jiajie]update configuration

--- src/main/java/com/example/demo/service/EducationService.java
+++ src/main/java/com/example/demo/service/EducationService.java
@@ -9,18 +9,17 @@ import com.example.demo.selfexception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class EducationService {
     private final EducationRepository educationRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final AtomicLong educationIdSeq = new AtomicLong();
 
-    public EducationService(EducationRepository educationRepository, UserRepository userRepository) {
+    public EducationService(EducationRepository educationRepository, UserService userService) {
         this.educationRepository = educationRepository;
-        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public List<Education> getEducationsByUserId(Long id) {
@@ -28,8 +27,7 @@ public class EducationService {
     }
 
     public Education createEducation(Long id, EducationRequestDTO educationRequestDTO) {
-        User user = userRepository.findOneById(id)
-                .orElseThrow(() -> new UserNotFoundException("User Not Found!"));
+        User user = userService.findById(id);
 
         Education education = new Education(
                 educationIdSeq.incrementAndGet(),
